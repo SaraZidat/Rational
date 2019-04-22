52667e1125999374e9d73612102f21cadf77c898
ZIDAT <zidatsara@gmail.com>
2019-04-22 20:21:50 +0200

    exercice9

--- src/Rational.java
+++ src/Rational.java
@@ -9,7 +9,7 @@ public class Rational {
         int d1 = Integer.parseInt(args[2]);
         int n2 = Integer.parseInt(args[3]);
         int d2 = Integer.parseInt(args[5]);
-        Rational r = multiply(new Rational(n1, d1), new Rational(n2, d2));
+        Rational r = divide(new Rational(n1, d1), new Rational(n2, d2));
 
         //String result = Integer.toString(r.numerator) + "/" + Integer.toString(r.denominator);
 
@@ -36,13 +36,13 @@ public class Rational {
         return (Integer.toString(numerator) + "/" + Integer.toString(denominator));
     }*/
 
-    private static Rational multiply(Rational r1, Rational r2){
+    private static Rational divide(Rational r1, Rational r2){
         int n1 = r1.numerator;
         int d1 = r1.denominator;
         int n2 = r2.numerator;
         int d2 = r2.denominator;
-        int numerator = n1 * n2;
-        int denominator = d1 * d2;
+        int numerator = n1 * d2;
+        int denominator = d1 * n2;
         int pgcd = computeGcd(numerator,denominator);
 
         if(numerator * denominator < 0){
